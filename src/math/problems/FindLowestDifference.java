fe2986a65fad0e4cdd52d76a8256c5c9409741ab
selmanHa <selmanhajii@gmail.com>
2019-09-08 12:16:15 -0500

    Math Find difference

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,5 +1,8 @@
 package math.problems;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class FindLowestDifference {
     public static void main(String[] args) {
         /*
@@ -11,8 +14,36 @@ public class FindLowestDifference {
         int [] array2 = {18,25,41,47,17,36,14,19};
 
 
+        int num1 = array1.length;
+        int num2 = array2.length;
+
+        System.out.println("The lowest difference between numbers in both arrays is "+ findLowestDifference(array1,
+                array2,num1,num2));
 
 
+    }
+
+    static int findLowestDifference(int array1[], int array2[], int num1, int num2){
+
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+
+        int temp1=0;
+        int temp2 = 0;
+
+        int finalSolution = Integer.MAX_VALUE; //assigning the max value
+
+        while( temp1<num1 && temp2<num2){
+            if (Math.abs(array1[temp1])-array2[temp2] < finalSolution){
+                finalSolution = Math.abs(array1[temp1]-array2[temp2]);
+            }
+            if (array1[temp1]<array2[temp2]){
+                temp1++;
+            }else {
+                temp2++;
+            }
+        }
+        return finalSolution;
     }
 
 }
