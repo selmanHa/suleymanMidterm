5864d0e459a3d88e4a546e0de213c9ab8e311195
selmanHa <selmanhajii@gmail.com>
2019-09-08 12:35:04 -0500

    Math Find missing number

--- src/math/problems/FindMissingNumber.java
+++ src/math/problems/FindMissingNumber.java
@@ -1,5 +1,7 @@
 package math.problems;
 
+import java.util.Arrays;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -12,6 +14,14 @@ public class FindMissingNumber {
          * Write java code to find the missing number from the array. Write static helper method to find it.
          */
          int [] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+
+         int lastNumer = 10;
+         int sumOfProductInHalf = (lastNumer*(lastNumer+1)/2); //10 * 11 +1 divide by 2 = 55
+         int sumOfNumbersInArray = Arrays.stream(array).sum(); //46
+
+         int numberMissed = sumOfProductInHalf - sumOfNumbersInArray; //55 - 46 = 9
+        System.out.println("The missing number in the array is "+numberMissed);
+
 
     }
 }
