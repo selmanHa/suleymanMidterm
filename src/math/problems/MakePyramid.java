0923279a4b98d6e518756a008a9bf40300a246b7
selmanHa <selmanhajii@gmail.com>
2019-09-08 13:19:09 -0500

    Drawing Pyramid

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -14,6 +14,19 @@ public class MakePyramid {
 
         */
 
+        for(int a=0; a<6; a++) { //a is less than the numbers of rows
 
+            for(int b=0; b<6 -a; b++) { //b is less than the numbers of rows
+
+                System.out.print(" "); //print space in between
+            }
+            for(int c=0; c<=a; c++) { //since c isn't greater than a, increment c by 1
+
+                System.out.print("* "); //printing the * in pyramid shape
+            }
+            System.out.println();
         }
-  }
+
+
+    }
+}
