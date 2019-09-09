c03ad6ac8e9ed3f3851b74c40fe565bb5d8b5e55
selmanHa <selmanhajii@gmail.com>
2019-09-08 19:02:06 -0500

    Using Queue using add, peek, remove

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -9,6 +13,22 @@ public class UseQueue {
 		 * 
 		 */
 
+		Queue<String> qu= new LinkedList<String>();
+
+		//add
+		qu.add("First Name: ");
+		qu.add("Last Name: ");
+		qu.add("ID: ");
+		qu.add("Email: ");
+
+		Iterator<String> it = qu.iterator();
+
+		while (it.hasNext()){
+			System.out.println(it.next()+"_______________");
+		}
+
+		System.out.println("\nThe head of the Queue is: "+qu.peek());
+		System.out.println("                       ================");
 	}
 
 }
